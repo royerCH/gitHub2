f6a27992f9e119823714df3e1b7844420f2aaed1
miguelUlloa13 <miguel.bmangelulloa13@gmail.com>
2023-02-22 15:02:15 -0600

    se agrego el botón clic here

--- gitHub2/ViewController.swift
+++ gitHub2/ViewController.swift
@@ -8,11 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var  array = ["Rogelio", "Larios"]
 
+    
+    @IBOutlet weak var btnIniciar: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+  
+        
     }
+    
+    
+    
+
+    @IBAction func btnIni(_ sender: Any) {
+        
+        print(array)
+    }
+    
+
+   
 
 
 }
